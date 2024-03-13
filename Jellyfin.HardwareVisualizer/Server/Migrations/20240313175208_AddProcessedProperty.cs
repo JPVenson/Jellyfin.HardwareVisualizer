35f0bae622aa16ca265ec15e9280bb115d3fbdd0
Venson <info@jpb.email>
2024-03-13 19:52:20 +0200

    Added Migration

---
+++ Jellyfin.HardwareVisualizer/Server/Migrations/20240313175208_AddProcessedProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jellyfin.HardwareVisualizer.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessedProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Processed",
+                table: "HardwareSurveyEntries",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Processed",
+                table: "HardwareSurveyEntries");
+        }
+    }
+}
