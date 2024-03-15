3303fe415271436d78ba9787377401fe727441f7
Venson <info@jpb.email>
2024-03-15 15:08:21 +0200

    Updated to file scoped NS

--- Jellyfin.HardwareVisualizer/Server/Program.cs
+++ Jellyfin.HardwareVisualizer/Server/Program.cs
@@ -19,221 +19,220 @@ using Octokit;
 using Octokit.Internal;
 using ProductHeaderValue = Octokit.ProductHeaderValue;
 
-namespace Jellyfin.HardwareVisualizer.Server
+namespace Jellyfin.HardwareVisualizer.Server;
+
+public class Program
 {
-	public class Program
+	public static void Main(string[] args)
 	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			builder.Configuration
-				.AddEnvironmentVariables("PG_")
-				.AddEnvironmentVariables("JF_")
-				.AddEnvironmentVariables("GH_");
-			// Add services to the container.
+		var builder = WebApplication.CreateBuilder(args);
+		builder.Configuration
+			.AddEnvironmentVariables("PG_")
+			.AddEnvironmentVariables("JF_")
+			.AddEnvironmentVariables("GH_");
+		// Add services to the container.
 			
 
-			string GetConnectionString()
+		string GetConnectionString()
+		{
+			var efCoreOptions = builder.Configuration.Get<EFCoreOptions>();
+			var s = $"User ID={efCoreOptions.User};Password={efCoreOptions.Password};Host={efCoreOptions.Host};Port={efCoreOptions.Port};Database={efCoreOptions.Database};Pooling=true;";
+			return s;
+		}
+
+		builder.Services
+			.UseServiceDiscovery()
+			.FromAssembly(typeof(Program).Assembly)
+			.DiscoverOptions(builder.Configuration)
+			.FromAssembly(typeof(Program).Assembly)
+			.LocateServices();
+
+		builder.Services.AddMemoryCache();
+		builder.Services.AddResponseCaching();
+		builder.Services.AddResponseCompression();
+		builder.Services.AddControllersWithViews()
+			.AddJsonOptions(e =>
 			{
-				var efCoreOptions = builder.Configuration.Get<EFCoreOptions>();
-				var s = $"User ID={efCoreOptions.User};Password={efCoreOptions.Password};Host={efCoreOptions.Host};Port={efCoreOptions.Port};Database={efCoreOptions.Database};Pooling=true;";
-				return s;
-			}
+				e.JsonSerializerOptions.AllowTrailingCommas = true;
+				e.JsonSerializerOptions.DictionaryKeyPolicy = null;
+				e.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+				e.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+				e.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower, false));
+			});
+		builder.Services.AddRazorPages();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddHttpContextAccessor();
+		builder.Services.AddHangfire(e => 
+			e.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+				.UseSimpleAssemblyNameTypeSerializer()
+				.UseRecommendedSerializerSettings()
+				.UsePostgreSqlStorage(f => f.UseNpgsqlConnection(() => GetConnectionString()))
+		);
+		builder.Services.AddHangfireServer();
 
-			builder.Services
-				.UseServiceDiscovery()
-				.FromAssembly(typeof(Program).Assembly)
-				.DiscoverOptions(builder.Configuration)
-				.FromAssembly(typeof(Program).Assembly)
-				.LocateServices();
-
-			builder.Services.AddMemoryCache();
-			builder.Services.AddResponseCaching();
-			builder.Services.AddResponseCompression();
-			builder.Services.AddControllersWithViews()
-				.AddJsonOptions(e =>
+		builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter("fixed_submit", options =>
+		{
+			options.PermitLimit = 1;
+			options.Window = TimeSpan.FromHours(1);
+			options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+			options.QueueLimit = 1;
+		}));
+		builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter("fixed_metadata", options =>
+		{
+			options.PermitLimit = 10;
+			options.Window = TimeSpan.FromMinutes(1);
+			options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+			options.QueueLimit = 10;
+		}));
+		var authenticationBuilder = builder.Services
+			.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+			.AddCookie();
+		var oauthSettings = builder.Configuration.GetSection("GH").Get<GithubOauthOptions>();
+		if (oauthSettings != null)
+		{
+			Console.WriteLine("Add Github Authentication.");
+			authenticationBuilder.AddGitHub(options =>
 				{
-					e.JsonSerializerOptions.AllowTrailingCommas = true;
-					e.JsonSerializerOptions.DictionaryKeyPolicy = null;
-					e.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-					e.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
-					e.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.SnakeCaseLower, false));
-				});
-			builder.Services.AddRazorPages();
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddHttpContextAccessor();
-			builder.Services.AddHangfire(e => 
-				e.SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-					.UseSimpleAssemblyNameTypeSerializer()
-					.UseRecommendedSerializerSettings()
-					.UsePostgreSqlStorage(f => f.UseNpgsqlConnection(() => GetConnectionString()))
-				);
-			builder.Services.AddHangfireServer();
+					var oauthSettings = builder.Configuration.GetSection("GH").Get<GithubOauthOptions>();
+					options.ClientId = oauthSettings.ClientId;
+					options.ClientSecret = oauthSettings.ClientSecret;
+					options.Scope.Add("user:email");
+					options.Scope.Add("read:org");
 
-			builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter("fixed_submit", options =>
-			{
-				options.PermitLimit = 1;
-				options.Window = TimeSpan.FromHours(1);
-				options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-				options.QueueLimit = 1;
-			}));
-			builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter("fixed_metadata", options =>
-			{
-				options.PermitLimit = 10;
-				options.Window = TimeSpan.FromMinutes(1);
-				options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
-				options.QueueLimit = 10;
-			}));
-			var authenticationBuilder = builder.Services
-				.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-				.AddCookie();
-			var oauthSettings = builder.Configuration.GetSection("GH").Get<GithubOauthOptions>();
-			if (oauthSettings != null)
-			{
-				Console.WriteLine("Add Github Authentication.");
-				authenticationBuilder.AddGitHub(options =>
+					options.SaveTokens = true;
+					options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+					options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+					options.ClaimActions.MapJsonKey("urn:github:login", "login");
+					options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+					options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+
+
+					options.Events.OnCreatingTicket = async context =>
 					{
-						var oauthSettings = builder.Configuration.GetSection("GH").Get<GithubOauthOptions>();
-						options.ClientId = oauthSettings.ClientId;
-						options.ClientSecret = oauthSettings.ClientSecret;
-						options.Scope.Add("user:email");
-						options.Scope.Add("read:org");
-
-						options.SaveTokens = true;
-						options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-						options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-						options.ClaimActions.MapJsonKey("urn:github:login", "login");
-						options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
-						options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-
-
-						options.Events.OnCreatingTicket = async context =>
+						var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+						request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+						request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+						var response = await context.Backchannel.SendAsync(request,
+							HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+						response.EnsureSuccessStatusCode();
+						var json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+						context.RunClaimActions(json.RootElement);
+						var octClient =
+							new GitHubClient(new ProductHeaderValue("Jellyfin.HardwareSurvey.Server"), new InMemoryCredentialStore(new Credentials(context.AccessToken)));
+						var organizations = await octClient.Organization.GetAllForCurrent();
+						if (organizations.All(e => e.Id != 45698031))
 						{
-							var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-							request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-							request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-							var response = await context.Backchannel.SendAsync(request,
-								HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-							response.EnsureSuccessStatusCode();
-							var json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
-							context.RunClaimActions(json.RootElement);
-							var octClient =
-								new GitHubClient(new ProductHeaderValue("Jellyfin.HardwareSurvey.Server"), new InMemoryCredentialStore(new Credentials(context.AccessToken)));
-							var organizations = await octClient.Organization.GetAllForCurrent();
-							if (organizations.All(e => e.Id != 45698031))
-							{
-								context.Fail("Not part of the Jellyfin Organization");
-								return;
-							}
-						};
-					})
-					;
-			}
-			else
-			{
-				Console.WriteLine("Github Authentication is not enabled.");
-			}
+							context.Fail("Not part of the Jellyfin Organization");
+							return;
+						}
+					};
+				})
+				;
+		}
+		else
+		{
+			Console.WriteLine("Github Authentication is not enabled.");
+		}
 
 				
 
-			builder.Services.AddSwaggerGen(options =>
+		builder.Services.AddSwaggerGen(options =>
+		{
+			options.SwaggerDoc("v1", new OpenApiInfo
 			{
-				options.SwaggerDoc("v1", new OpenApiInfo
+				Version = "v1",
+				Title = "Jellyfin Hardware Survey API",
+				Description = "Web API for storing Hardware Survey results for the Jellyfin Project",
+				TermsOfService = new Uri("https://example.com/terms"),
+				Contact = new OpenApiContact
 				{
-					Version = "v1",
-					Title = "Jellyfin Hardware Survey API",
-					Description = "Web API for storing Hardware Survey results for the Jellyfin Project",
-					TermsOfService = new Uri("https://example.com/terms"),
-					Contact = new OpenApiContact
-					{
-						Name = "Jellyfin Contact",
-						Url = new Uri("https://jellyfin.org/contact")
-					},
-					License = new OpenApiLicense
-					{
-						Name = "GNU GENERAL PUBLIC LICENSE",
-						Url = new Uri("https://github.com/joshuaboniface/hwatest/blob/master/LICENSE")
-					}
-				});
-
-				options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+					Name = "Jellyfin Contact",
+					Url = new Uri("https://jellyfin.org/contact")
+				},
+				License = new OpenApiLicense
 				{
-					Description = "ApiKey must appear in header",
-					Type = SecuritySchemeType.ApiKey,
-					Name = "XApiKey",
-					In = ParameterLocation.Header,
-					Scheme = "ApiKeyScheme"
-				});
-				var key = new OpenApiSecurityScheme()
-				{
-					Reference = new OpenApiReference
-					{
-						Type = ReferenceType.SecurityScheme,
-						Id = "ApiKey"
-					},
-					In = ParameterLocation.Header
-				};
-				var requirement = new OpenApiSecurityRequirement
-				{
-					{ key, new List<string>() }
-				};
-				options.AddSecurityRequirement(requirement);
-
-				var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-				options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+					Name = "GNU GENERAL PUBLIC LICENSE",
+					Url = new Uri("https://github.com/joshuaboniface/hwatest/blob/master/LICENSE")
+				}
 			});
 
-			builder.Services.AddDbContextFactory<HardwareVisualizerDataContext>(opt =>
+			options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
 			{
-				var connectionString = GetConnectionString();
-				opt.UseNpgsql(connectionString);
+				Description = "ApiKey must appear in header",
+				Type = SecuritySchemeType.ApiKey,
+				Name = "XApiKey",
+				In = ParameterLocation.Header,
+				Scheme = "ApiKeyScheme"
 			});
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
+			var key = new OpenApiSecurityScheme()
 			{
-				app.UseWebAssemblyDebugging();
-			}
-			else
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "ApiKey"
+				},
+				In = ParameterLocation.Header
+			};
+			var requirement = new OpenApiSecurityRequirement
 			{
-				app.UseExceptionHandler("/Error");
-			}
+				{ key, new List<string>() }
+			};
+			options.AddSecurityRequirement(requirement);
 
-			app.UseResponseCaching();
-			app.UseResponseCompression();
-			app.UseBlazorFrameworkFiles();
-			app.UseStaticFiles();
+			var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+			options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+		});
 
-			app.UseRouting();
-			app.UseAuthentication();
-			app.UseAuthorization();
+		builder.Services.AddDbContextFactory<HardwareVisualizerDataContext>(opt =>
+		{
+			var connectionString = GetConnectionString();
+			opt.UseNpgsql(connectionString);
+		});
 
-			app.UseSwagger();
-			app.UseSwaggerUI(options =>
-			{
-				options.OAuthClientId("api-swagger");
-				options.OAuthScopes("profile", "openid", "api");
-				options.OAuthUsePkce();
-				options.EnablePersistAuthorization();
-			});
+		var app = builder.Build();
 
-			app.MapRazorPages();
-			app.MapControllers();
-			app.MapFallbackToFile("index.html");
-			app.MapSwagger();
-
-			using (var scope = app.Services.CreateScope())
-			{
-				var services = scope.ServiceProvider;
-
-				var context = services.GetRequiredService<HardwareVisualizerDataContext>();
-				context.Database.Migrate();
-			}
-
-
-			app.Run();
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseWebAssemblyDebugging();
 		}
+		else
+		{
+			app.UseExceptionHandler("/Error");
+		}
+
+		app.UseResponseCaching();
+		app.UseResponseCompression();
+		app.UseBlazorFrameworkFiles();
+		app.UseStaticFiles();
+
+		app.UseRouting();
+		app.UseAuthentication();
+		app.UseAuthorization();
+
+		app.UseSwagger();
+		app.UseSwaggerUI(options =>
+		{
+			options.OAuthClientId("api-swagger");
+			options.OAuthScopes("profile", "openid", "api");
+			options.OAuthUsePkce();
+			options.EnablePersistAuthorization();
+		});
+
+		app.MapRazorPages();
+		app.MapControllers();
+		app.MapFallbackToFile("index.html");
+		app.MapSwagger();
+
+		using (var scope = app.Services.CreateScope())
+		{
+			var services = scope.ServiceProvider;
+
+			var context = services.GetRequiredService<HardwareVisualizerDataContext>();
+			context.Database.Migrate();
+		}
+
+
+		app.Run();
 	}
 }
--- Jellyfin.HardwareVisualizer/Shared/Models/TestDataRequestModel.cs
+++ Jellyfin.HardwareVisualizer/Shared/Models/TestDataRequestModel.cs
@@ -1,82 +1,81 @@
-﻿namespace Jellyfin.HardwareVisualizer.Shared.Models
+﻿namespace Jellyfin.HardwareVisualizer.Shared.Models;
+
+public class PlatformData
 {
-	public class PlatformData
-	{
-		public SupportedPlatform[] Platforms { get; set; }
-	}
+	public SupportedPlatform[] Platforms { get; set; }
+}
 
-	public class SupportedPlatform
-	{
-		public Guid Id { get; set; }
-		public string Name { get; set; }
-		public string Type { get; set; }
-		public string Version { get; set; }
-		public string VersionId { get; set; }
-		public string DisplayName { get; set; }
-		public string ReplacementId { get; set; }
-		public bool Supported { get; set; }
-	}
+public class SupportedPlatform
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; }
+	public string Type { get; set; }
+	public string Version { get; set; }
+	public string VersionId { get; set; }
+	public string DisplayName { get; set; }
+	public string ReplacementId { get; set; }
+	public bool Supported { get; set; }
+}
 
-	public class TestDataRequestModel
-	{
-		public string Token { get; set; }
+public class TestDataRequestModel
+{
+	public string Token { get; set; }
 
-		public FfmpegModel Ffmpeg { get; set; }
+	public FfmpegModel Ffmpeg { get; set; }
 
-		public IEnumerable<TestCaseModel> Tests { get; set; }
+	public IEnumerable<TestCaseModel> Tests { get; set; }
 
-	}
+}
 
-	public class FfmpegModel
-	{
-		public string FfmpegSourceUrl { get; set; }
-		public string FfmpegVersion { get; set; }
-		public IEnumerable<ExternalFileHashModel> FfmpegHashs { get; set; }
-	}
+public class FfmpegModel
+{
+	public string FfmpegSourceUrl { get; set; }
+	public string FfmpegVersion { get; set; }
+	public IEnumerable<ExternalFileHashModel> FfmpegHashs { get; set; }
+}
 
-	public class TestCaseModel
-	{
-		public string Name { get; set; }
-		public string SourceUrl { get; set; }
-		public IEnumerable<ExternalFileHashModel> SourceHashs { get; set; }
-		public TestCaseType TestType { get; set; }
-		public IEnumerable<TestCaseDataModel> Data { get; set; }
+public class TestCaseModel
+{
+	public string Name { get; set; }
+	public string SourceUrl { get; set; }
+	public IEnumerable<ExternalFileHashModel> SourceHashs { get; set; }
+	public TestCaseType TestType { get; set; }
+	public IEnumerable<TestCaseDataModel> Data { get; set; }
 
-	}
+}
 
-	public class TestCaseDataModel
-	{
-		public string Id { get; set; }
-		public string FromResolution { get; set; }
-		public string ToResolution { get; set; }
-		public long Bitrate { get; set; }
+public class TestCaseDataModel
+{
+	public string Id { get; set; }
+	public string FromResolution { get; set; }
+	public string ToResolution { get; set; }
+	public long Bitrate { get; set; }
 
-		public IEnumerable<FfmpegArgumentsModel> Arguments { get; set; }
-	}
+	public IEnumerable<FfmpegArgumentsModel> Arguments { get; set; }
+}
 
-	public class FfmpegArgumentsModel
-	{
-		public FfmpegArgumentDeviceType Type { get; set; }
-		public string Args { get; set; }
-		public string Codec { get; set; }
-	}
+public class FfmpegArgumentsModel
+{
+	public FfmpegArgumentDeviceType Type { get; set; }
+	public string Args { get; set; }
+	public string Codec { get; set; }
+}
 
-	public enum FfmpegArgumentDeviceType
-	{
-		Cpu,
-		Nvidia,
-		Amd,
-		Intel
-	}
+public enum FfmpegArgumentDeviceType
+{
+	Cpu,
+	Nvidia,
+	Amd,
+	Intel
+}
 
-	public enum TestCaseType
-	{
-		Transcode
-	}
+public enum TestCaseType
+{
+	Transcode
+}
 
-	public class ExternalFileHashModel
-	{
-		public string Type { get; set; }
-		public string Hash { get; set; }
-	}
+public class ExternalFileHashModel
+{
+	public string Type { get; set; }
+	public string Hash { get; set; }
 }
